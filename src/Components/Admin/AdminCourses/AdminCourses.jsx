23a1aa93490be97c541f9bb28222aac27d14f921
carlavega13 <carlavega231323@gmail.com>
2023-10-31 20:16:31 -0300

    edit css

--- src/Components/Admin/AdminCourses/AdminCourses.jsx
+++ src/Components/Admin/AdminCourses/AdminCourses.jsx
@@ -44,7 +44,7 @@ const columns=[
           return (
             <div style={{display:"flex", justifyContent: "space-between", alignItems:"center", width: "300px"}}>
             <a  href={`https://wa.me/${phoneNumber}`} target="_blank" rel="noopener noreferrer">
-              <p>{phoneNumber}</p><BsWhatsapp style={{color: "#2f3367"}} />
+             <BsWhatsapp style={{color: "#2f3367"}} />
               
             </a>
           </div>
